a5b9eef28655594084e8b2411ea9977d4db1c11f
jdenoc <jdenoc@gmail.com>
2017-03-07 11:42:35 -0600

    Added/adjusted logic in the EntryController to handle filtering entries by parameters passed via a POST request.

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,11 @@ Route::get('tags', 'Api\TagController@get_tags');                               
 Route::get('accounts', 'Api\AccountController@get_accounts');                       // GET /api/accounts
 Route::get('account/{account_id}', 'Api\AccountController@get_account');            // GET /api/account/{account_id}
 Route::get('entry/{entry_id}', 'Api\EntryController@get_entry');                    // GET /api/entry/{entry_id}
-Route::get('entries', 'Api\EntryController@get_entries');                           // GET /api/entries
+Route::get('entries', 'Api\EntryController@get_paged_entries');                     // GET /api/entries
 Route::get('entries/{page}', 'Api\EntryController@get_paged_entries');              // GET /api/entries/{page}
 Route::delete('attachment/{uuid}', 'Api\AttachmentController@delete_attachment');   // DELETE /api/attachment/{uuid}
 Route::delete('entry/{entry_id}', 'Api\EntryController@delete_entry');              // DELETE /api/entry/{entry_id}
 Route::post('entry', 'Api\EntryController@create_entry');                           // POST /api/entry
 Route::put('entry/{entry_id}', 'Api\EntryController@update_entry');                 // PUT /api/entry/{entry_id}
+Route::post('entries', 'Api\EntryController@filter_paged_entries');
+Route::post('entries/{page}', 'Api\EntryController@filter_paged_entries');
