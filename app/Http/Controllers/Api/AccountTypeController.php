d3e23c2f748124ca06575675dd3890d9f93305a3
jdenoc <jdenoc@gmail.com>
2017-03-19 19:37:58 -0500

    new feature: `GET /api/account-types` to retrieve all account_types

--- app/Http/Controllers/Api/AccountTypeController.php
+++ app/Http/Controllers/Api/AccountTypeController.php
@@ -9,6 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AccountTypeController extends Controller {
 
+    public function list_account_types(){
+        $account_types = AccountType::all();
+        if(is_null($account_types) || $account_types->isEmpty()){
+            return response([], Response::HTTP_NOT_FOUND);
+        } else {
+            $account_types->makeHidden('last_updated');
+            return response($account_types, Response::HTTP_OK);
+        }
+    }
+
     public function disable_account_type($account_type_id){
         $account_type = AccountType::find($account_type_id);
         if(empty($account_type)){
