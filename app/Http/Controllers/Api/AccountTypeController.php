39e6ef098d4a19a04dd09690d2bf8f2cbfe5bb37
jdenoc <jdenoc@gmail.com>
2017-05-03 14:17:17 -0500

    - Changed Symfony\Component\HttpFoundation\Response to be able to utilise HttpStatus as the classname in the Api\AccountTypeController and it's tests. - GET /api/account-types responses now return a count. Now testing for it.

--- app/Http/Controllers/Api/AccountTypeController.php
+++ app/Http/Controllers/Api/AccountTypeController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\AccountType;
 use Carbon\Carbon;
 use App\Http\Controllers\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response as HttpStatus;
 
 class AccountTypeController extends Controller {
 
     public function list_account_types(){
         $account_types = AccountType::all();
         if(is_null($account_types) || $account_types->isEmpty()){
-            return response([], Response::HTTP_NOT_FOUND);
+            return response([], HttpStatus::HTTP_NOT_FOUND);
         } else {
             return response($account_types, Response::HTTP_OK);
         }
@@ -21,13 +21,13 @@ class AccountTypeController extends Controller {
     public function disable_account_type($account_type_id){
         $account_type = AccountType::find($account_type_id);
         if(empty($account_type)){
-            return response('', Response::HTTP_NOT_FOUND);
+            return response('', HttpStatus::HTTP_NOT_FOUND);
         }
 
         $account_type->disabled = true;
         $account_type->disabled_stamp = Carbon::now()->toDateTimeString();
         $account_type->save();
-        return response('', Response::HTTP_NO_CONTENT);
+        return response('', HttpStatus::HTTP_NO_CONTENT);
     }
 
 }
