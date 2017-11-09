45150722bfec084dcd582c98d7208756b7eb8c59
jdenoc <jdenoc@gmail.com>
2017-11-09 09:50:48 -0600

    Merging `$_ENV` and variables names from `.env` file for a better overview of what environment variables are currently used.

--- app/Console/Commands/TravisCi.php
+++ app/Console/Commands/TravisCi.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class TravisCi extends Command {
 
@@ -35,8 +36,10 @@ class TravisCi extends Command {
         foreach($this->options() as $command_option){
             switch($command_option){
                 case 'display-env':
-                    $this->info("Environment Variables:");
-                    $env_values = array_map([$this, 'output_environment_variable_and_value'], array_keys($_ENV));
+                    $this->comment("Environment Variables:");
+                    $env_names = array_merge(array_keys($_ENV), $this->get_env_names_from_dotenv());
+                    $env_names = array_unique($env_names);
+                    $env_values = array_map([$this, 'output_environment_variable_and_value'], $env_names);
                     $this->table(['variable', 'value'], $env_values);
                     $this->line('');    // new line after output in case we have other output
                     break;
@@ -44,8 +47,29 @@ class TravisCi extends Command {
         }
     }
 
+    /**
+     * @param string $variable_name
+     * @return array
+     */
     private function output_environment_variable_and_value($variable_name){
         return ['variable'=>$variable_name, 'value'=>env($variable_name)];
+    }
+
+    /**
+     * @return array
+     */
+    private function get_env_names_from_dotenv(){
+        $dot_env_variable_names = [];
+        if(File::exists(base_path('.env'))){
+            $dot_env_contents = File::get(base_path('.env'));
+            $dot_env_content_lines = explode("\n", $dot_env_contents);
+            foreach($dot_env_content_lines as $dot_env_content_line){
+                if(!empty($dot_env_content_line) && strpos($dot_env_content_line, '#') !== 0){
+                    $dot_env_variable_names[] = substr($dot_env_content_line, 0, strpos($dot_env_content_line, '='));
+                }
+            }
+        }
+        return $dot_env_variable_names;
     }
 
 }
