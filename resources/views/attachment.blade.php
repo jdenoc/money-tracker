fb70fb06e3e6b5093dcc9202b2e0529681cd90c9
jdenoc <jdenoc@gmail.com>
2018-01-28 22:11:56 -0600

    Revert "feature/bower-to-yarn"

--- resources/views/attachment.blade.php
+++ resources/views/attachment.blade.php
@@ -1,9 +1,9 @@
 <html>
     <head>
     <title>Attachment</title>
-    <link href="{{asset('css/attachment/'.$uuid)}}" type="text/css" rel="stylesheet" />
+    <link href='/css/attachment/{{$uuid}}' type='text/css' rel='stylesheet' />
 </head>
-<body oncontextmenu="return false;">
-    <img src="{{asset('imgs/foreground.png')}}" alt="attachment"/>
+<body oncontextmenu='return false;'>
+    <img src='/imgs/foreground.png' alt='attachment'/>
 </body>
 </html>
