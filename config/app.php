2b8cfa739f218c24fbb3fa47d3ae5ab15e8b416a
jdenoc <jdenoc@gmail.com>
2018-02-13 03:30:34 -0600

    Pulling stuff from laravel 5.4.30

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Money Tracker',
+    'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
