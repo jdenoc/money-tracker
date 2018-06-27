f82559e3e1ec1b1cb7fed7bca9b7eb6ff0398754
jdenoc <jdenoc@gmail.com>
2018-06-27 11:30:51 -0500

    Created vuejs components to display the entries table.

---
+++ resources/assets/js/components/entries-table-entry-row.vue
@@ -0,0 +1,62 @@
+<template>
+    <tr
+        v-bind:class="{'has-background-warning': confirm && expense, 'has-background-success': confirm && !expense, 'has-text-grey-light' : isFutureEntry}">
+        <td><button class="button is-inverted is-info fas fa-edit" v-on:click="openEditEntryModal(id)"></button></td>
+        <td v-text="date"></td>
+        <td v-text="memo"></td>
+        <td class="has-text-right">
+            <span v-if="expense"></span>
+            <span v-else>${{value}}</span>
+        </td>
+        <td class="has-text-right">
+            <span v-if="expense">${{value}}</span>
+            <span v-else></span>
+        </td>
+        <td v-text="accountTypeName"></td>
+        <td><i v-bind:class="{ 'far fa-square': !hasAttachments, 'fas fa-check-square': hasAttachments }"></i></td>
+        <td><div class="tags">
+            <span class="tag is-rounded" v-for="tagId in tagIds" v-text="getTagName(tagId)"></span>
+        </div></td>
+    </tr>
+</template>
+
+<script>
+    import {AccountTypes} from "../account-types";
+    import {Tags} from "../tags";
+
+    export default {
+        name: "entries-table-entry-row",
+        props: ['id', 'date', 'accountTypeId', 'value', 'memo', 'expense', 'confirm', 'disabled', 'hasAttachments', 'tagIds'],
+        data: function(){
+            return {
+                millisecondsPerMinute: 60000
+            }
+        },
+        computed: {
+            isFutureEntry: function(){
+                let timezoneOffset = new Date().getTimezoneOffset()*this.millisecondsPerMinute;
+                return Date.parse(this.date)+timezoneOffset > Date.now();
+            },
+            accountTypeName: function(){
+                return new AccountTypes().getNameById(this.accountTypeId);
+            },
+        },
+        methods: {
+            getTagName: function(tagId){
+                return new Tags().getNameById(tagId);
+            },
+            openEditEntryModal: function(entryId){
+                this.modalNotAvailable(entryId);
+            },
+            modalNotAvailable: function(entryId){
+                alert("Modal not currently available\nEntry "+entryId+" can't be modified at this time");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .tags{
+        max-width: 250px;
+    }
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ mix.js('resources/assets/js/app.js', 'public/vue/js')
     // font-awesome
     .copy('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/vue/css/font-awesome.css')
     .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2', 'public/vue/webfonts/fa-solid-900.woff2')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2', 'public/vue/webfonts/fa-regular-400.woff2')
     // bulma-accordion
     .js('node_modules/bulma-accordion/dist/bulma-accordion.js', 'public/vue/js')
     .copy('node_modules/bulma-accordion/dist/bulma-accordion.min.css', 'public/vue/css/bulma-accordion.css')
