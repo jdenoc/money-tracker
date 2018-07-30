42bbcd0b9692f24879379e7203bfdfe82a054ba3
jdenoc <jdenoc@gmail.com>
2018-07-30 11:23:10 -0500

    Added tags-input field to the entry-modal.

--- webpack.mix.js
+++ webpack.mix.js
@@ -30,5 +30,7 @@ mix.js('resources/assets/js/app.js', jsDirectory)
     .copy(nodeDirectory+'/@voerro/vue-tagsinput/dist/style.css', cssDirectory+'/tags-input.css')
     // bulma-checkradio
     .copy(nodeDirectory+'/bulma-checkradio/dist/css/bulma-checkradio.min.css', cssDirectory+'/bulma-checkradio.css')
+    // tags-input
+    .copy(nodeDirectory+'/@voerro/vue-tagsinput/dist/style.css', cssDirectory+'/tags-input.css')
 
     .sass('resources/assets/sass/app.scss', cssDirectory);
