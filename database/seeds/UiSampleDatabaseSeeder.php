c7269ac21f0bf6507afbc6252a55f20c813e7a3c
jdenoc <jdenoc@gmail.com>
2018-09-18 00:24:42 -0500

    Updated the UiSampeDatabaseSeeder to populate the database with a better selection of data.

--- database/seeds/UiSampleDatabaseSeeder.php
+++ database/seeds/UiSampleDatabaseSeeder.php
@@ -4,12 +4,13 @@ use Illuminate\Database\Seeder;
 
 class UiSampleDatabaseSeeder extends Seeder {
 
-    const COUNT_TAG = 3;
+    const COUNT_ACCOUNT = 2;
+    const COUNT_ACCOUNT_TYPE = 3;
+    const COUNT_ATTACHMENT = 5;
+    const COUNT_ENTRY = 5;
     const COUNT_INSTITUTION = 2;
-    const COUNT_ACCOUNT = 4;
-    const COUNT_ACCOUNT_TYPE = 6;
-    const COUNT_ENTRY = 10;
-    const COUNT_ATTACHMENT = 2;
+    const COUNT_MIN = 1;
+    const COUNT_TAG = 5;
 
     /**
      * Run the database seeds.
@@ -26,38 +27,77 @@ class UiSampleDatabaseSeeder extends Seeder {
         $institution_ids = $institutions->pluck('id')->toArray();
 
         $accounts = collect();
-        for($account_i=0; $account_i<self::COUNT_ACCOUNT; $account_i++){
-            $account = factory(App\Account::class)->create(['institution_id'=>$faker->randomElement($institution_ids)]);
-            $accounts->push($account);
-            unset($account);
+        foreach($institution_ids as $institution_id){
+            $accounts = $this->addAccountToCollection($accounts, ['institution_id'=>$institution_id, 'disabled'=>false]);
         }
+        $accounts = $this->addAccountToCollection($accounts, ['institution_id'=>$faker->randomElement($institution_ids), 'disabled'=>true]);
         $account_ids = $accounts->pluck('id')->toArray();
 
         $account_types = collect();
-        for($account_type_i=0; $account_type_i<self::COUNT_ACCOUNT_TYPE; $account_type_i++){
-            $account_type = factory(App\AccountType::class)->create(['account_id'=>$faker->randomElement($account_ids)]);
-            $account_types->push($account_type);
-            unset($account_type);
+        foreach($account_ids as $account_id){
+            $account_types = $this->addAccountTypeToCollection($account_types, ['account_id'=>$account_id, 'disabled'=>false], $faker);
         }
+        $account_types = $this->addAccountTypeToCollection($account_types, ['account_id'=>$faker->randomElement($account_ids), 'disabled'=>true], $faker);
         $account_type_ids = $account_types->pluck('id')->toArray();
 
         $entries = collect();
-        for($entry_i=0; $entry_i<self::COUNT_ENTRY; $entry_i++){
-            $entry = factory(App\Entry::class)->create(['account_type_id'=>$faker->randomElement($account_type_ids)]);
+        foreach($account_type_ids as $account_type_id){
+            $entries = $this->addEntryToCollection($entries, ['account_type_id'=>$account_type_id, 'disabled'=>false], $faker);
+        }
+        $entries = $this->addEntryToCollection($entries, ['account_type_id'=>$faker->randomElement($account_type_ids), 'disabled'=>true], $faker);
+        $entry_ids = $entries->pluck('id')->toArray();
+
+        foreach($entries as $entry){
             if($faker->boolean){    // randomly assign tags to entries
-                $entry_tag_ids = $faker->randomElements($tag_ids, $faker->numberBetween(1, self::COUNT_TAG));
+                $entry_tag_ids = $faker->randomElements($tag_ids, $faker->numberBetween(self::COUNT_MIN, self::COUNT_TAG));
                 $entry->tags()->attach($entry_tag_ids);
             }
-            $entries->push($entry);
-            unset($entry, $entry_tag_ids);
         }
-        $entry_ids = $entries->pluck('id')->toArray();
 
         for($attachment_i=0; $attachment_i<self::COUNT_ATTACHMENT; $attachment_i++){
             factory(App\Attachment::class)->create(['entry_id'=>$faker->randomElement($entry_ids)]);
         }
+    }
 
+    /**
+     * @param Illuminate\Support\Collection $account_collection
+     * @param array $data
+     * @return Illuminate\Support\Collection
+     */
+    private function addAccountToCollection($account_collection, $data){
+        return $this->addToCollection($account_collection, App\Account::class, $data, self::COUNT_ACCOUNT);
+    }
 
+    /**
+     * @param Illuminate\Support\Collection $account_type_collection
+     * @param array $data
+     * @param Faker\Generator $faker
+     * @return \Illuminate\Support\Collection
+     */
+    private function addAccountTypeToCollection($account_type_collection, $data, $faker){
+        return $this->addToCollection($account_type_collection, App\AccountType::class, $data, $faker->numberBetween(self::COUNT_MIN, self::COUNT_ACCOUNT_TYPE));
+    }
+
+    /**
+     * @param Illuminate\Support\Collection $entry_collection
+     * @param array $data
+     * * @param Faker\Generator $faker
+     * @return \Illuminate\Support\Collection
+     */
+    private function addEntryToCollection($entry_collection, $data, $faker){
+        return $this->addToCollection($entry_collection, App\Entry::class, $data, $faker->numberBetween(self::COUNT_MIN, self::COUNT_ENTRY));
+    }
+
+    /**
+     * @param Illuminate\Support\Collection $collection
+     * @param $type_class
+     * @param array $data
+     * @param int $count
+     * @return Illuminate\Support\Collection mixed
+     */
+    private function addToCollection($collection, $type_class, $data, $count=1){
+        $object = factory($type_class, $count)->create($data);  // when passing a count value to a factory, a collection is ALWAYS returned
+        return $collection->merge($object);
     }
 
 }
