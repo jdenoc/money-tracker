e622a1e63adc971b6ff88c4549d5ee19f08a8bee
jdenoc <jdenoc@gmail.com>
2018-09-28 23:19:40 -0500

    Adding missing laravel/dusk test files.

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests;
+
+use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+
+abstract class DuskTestCase extends BaseTestCase {
+
+    use CreatesApplication;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare(){
+        static::startChromeDriver();
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver(){
+        return RemoteWebDriver::create(
+             'http://selenium:4444/wd/hub', DesiredCapabilities::chrome(), 5000, 10000
+        );
+    }
+
+    public function getApiTags(){
+        $tags_response = $this->get("/api/tags");
+        $tags = $tags_response->json();
+        unset($tags['count']);
+        return $tags;
+    }
+
+    public function getApiAccountTypes(){
+        $account_types_response = $this->get("/api/account-types");
+        $account_types = $account_types_response->json();
+        unset($account_types['count']);
+        return $account_types;
+    }
+
+}
