e10bb1fc9d076a792be7f2c9a2ae03ad8509b23c
jdenoc <jdenoc@gmail.com>
2019-02-15 21:20:19 -0600

    Made modifications to the `processLocaleDateForTyping` Browser macro function to better handle year values.

--- app/Providers/DuskServiceProvider.php
+++ app/Providers/DuskServiceProvider.php
@@ -43,6 +43,16 @@ class DuskServiceProvider extends DuskServiceProviderBase {
             }elseif(strpos($locale_date, '-') !== false){
                 $locale_date_components = explode('-', $locale_date);
             }
+
+            // we're assuming the year in the date provided will always be the last component
+            $year = end($locale_date_components);
+            if(strlen($year) == 2){    // this is assuming the year is the last component of the date
+                if($year >= 70 && $year <= 99){   // should never be a value before 1970
+                    $locale_date_components[count($locale_date_components)-1] = "19".$year;
+                } else {
+                    $locale_date_components[count($locale_date_components)-1] = "20".$year;
+                }
+            }
             foreach($locale_date_components as $key=>$date_component){
                 $locale_date_components[$key] = ($date_component < 10) ? '0'.$date_component : $date_component;
             }
