fb80ce729203b55bfdb95c5aeea868ba12a70d89
jdenoc <jdenoc@gmail.com>
2019-08-02 21:23:11 -0500

    made correction to `.docker/cmd/laravel-logs.sh` so that only files that exist will be "tail"ed

--- .docker/cmd/laravel-logs.sh
+++ .docker/cmd/laravel-logs.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker container exec -t app.money-tracker sh -c "for log in ''ls -d /var/www/money-tracker/storage/logs/*''; do echo \"\\n\\n[LOG]: \$log\"; tail -100 \$log; done"
+docker container exec -t app.money-tracker sh -c \
+  "for log in ''ls -d /var/www/money-tracker/storage/logs/*''; do
+    if [ -f \$log ]; then
+      echo \"[LOG]: \$log\"; tail -100 \$log;
+      echo \"\\n\";
+    fi
+  done"
