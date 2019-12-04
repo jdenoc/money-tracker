507c5d8e8c0b72d3d3aef0eed38fa52e69829930
jdenoc <jdenoc@gmail.com>
2019-12-04 11:41:09 -0600

    modified `.docker/cmd/laravel-logs.sh` to also output javascript console logs

--- .docker/cmd/laravel-logs.sh
+++ .docker/cmd/laravel-logs.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# generic laravel logs
 docker container exec -t app.money-tracker sh -c \
   "for log in ''ls -d /var/www/money-tracker/storage/logs/*''; do
+    if [ -f \$log ]; then
+      echo \"[LOG]: \$log\"; tail -100 \$log;
+      echo \"\\n\";
+    fi
+  done"
+
+# laravel dusk javascript console logs
+docker container exec -t app.money-tracker sh -c \
+  "for log in ''ls -d /var/www/money-tracker/tests/Browser/console/*''; do
     if [ -f \$log ]; then
       echo \"[LOG]: \$log\"; tail -100 \$log;
       echo \"\\n\";
