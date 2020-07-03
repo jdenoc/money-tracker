bca6188d8452dfd250a5f618f67cc0e89ea15ba7
jdenoc <jdenoc@gmail.com>
2020-07-02 19:43:20 -0500

    Put quotes (`"`) around a few variables in the `.docker/mysql-health-check.sh` script _to prevent glob matching_.

--- .docker/mysql-health-check.sh
+++ .docker/mysql-health-check.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-test1=$( mysqladmin ping --user=$MYSQL_USER --password=$MYSQL_PASSWORD \
+test1=$( mysqladmin ping --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" \
 	2>&1 | grep -v "Warning: Using a password" )
 
-test2=$( mysql --user=$MYSQL_USER --password=$MYSQL_PASSWORD --database=$MYSQL_DATABASE --skip-column-names  -e "SELECT NOW();" \
+test2=$( mysql --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --database="$MYSQL_DATABASE" --skip-column-names  -e "SELECT NOW();" \
 	2>&1 | grep -v "Warning: Using a password" )
 
 test1_ok="mysqld is alive"
 test2_ok=$( date +"%Y-%m-%d %H:%M:%S" )
 
-if [[ $test1 == $test1_ok ]]; then
-	if [[ $test2 == $test2_ok ]]; then
+if [[ $test1 == "$test1_ok" ]]; then
+	if [[ $test2 == "$test2_ok" ]]; then
 		echo OK;
 	else
 		# mysql connection denied
