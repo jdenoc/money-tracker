1b2a0ea6ace071e8b139264c810d7b6489185dad
jdenoc <jdenoc@gmail.com>
2020-08-05 18:34:04 -0500

    During the teardown step of _Dusk_ tests, we're going to truncate all tables prior to performing the database migration rollbacks. Should speed up tests.

--- tests/DuskWithMigrationsTestCase.php
+++ tests/DuskWithMigrationsTestCase.php
@@ -19,7 +19,9 @@ abstract class DuskWithMigrationsTestCase extends DuskTestCase {
      * This trait is not used in the DuskTestCase class for those instances
      * when a database migration is not required for said tests.
      */
-    use DatabaseMigrations;
+    use DatabaseMigrations {
+        runDatabaseMigrations as defaultRunDatabaseMigrations;
+    }
 
     /**
      * Often when a test involving a database fails, the failure is very database content dependent.
@@ -31,6 +33,27 @@ abstract class DuskWithMigrationsTestCase extends DuskTestCase {
     public function setUp(){
         parent::setUp();
         $this->seed('UiSampleDatabaseSeeder');  // run database seeder
+    }
+
+    /**
+     * Overriding the method from the DatabaseMigrations trait
+     */
+    public function runDatabaseMigrations(){
+        $this->beforeApplicationDestroyed(static function () {
+            // Truncate all tables, except migrations
+            // This will speed up tests
+            // This is supposed to occur before the migrate:rollback command
+            $tables = \DB::select('SHOW TABLES');
+
+            $db_name = config('database.connections.mysql.database');
+            $prop = "Tables_in_%s";
+            foreach ($tables as $table) {
+                $table_name = $table->{sprintf($prop, $db_name)};
+                if ($table_name !== 'migrations')
+                    \DB::table($table_name)->truncate();
+            }
+        });
+        $this->defaultRunDatabaseMigrations();
     }
 
 }
