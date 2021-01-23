8cbef8a8c128f76bcec60638fdd8a94229f83067
jdenoc <jdenoc@gmail.com>
2021-01-22 22:29:54 -0600

    Extracted the `artisan app:version` command into it's own command.

--- config/app.php
+++ config/app.php
@@ -16,17 +16,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Version
-    |--------------------------------------------------------------------------
-    |
-    | This value is the version of your application. This value is used to
-    | inform users what version of the app they are using.
-    */
-
-    'version' => env('APP_VERSION'),
-
-    /*
-    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
