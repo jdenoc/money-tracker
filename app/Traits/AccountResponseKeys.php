5936bcd956b3c717dc92b11f38f0ebf0fdae9dda
jdenoc <jdenoc@gmail.com>
2021-04-28 22:09:01 -0500

    Added API endpoints to create/update accounts.

---
+++ app/Traits/AccountResponseKeys.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+trait AccountResponseKeys {
+
+    private static $RESPONSE_KEY_ID = 'id';
+    private static $RESPONSE_KEY_ERROR = 'error';
+
+    private static $ERROR_ID = 0;
+    private static $ERROR_MSG_NO_ERROR = '';
+    private static $ERROR_MSG_NO_DATA = 'No data provided';
+    private static $ERROR_MSG_MISSING_PROPERTY = 'Missing data: %s';
+    private static $ERROR_MSG_DOES_NOT_EXIST = 'Account does not exist';
+    private static $ERROR_MSG_INVALID_INSTITUTION = 'Instition provided does not exist';
+
+    protected function fillMissingPropertyErrorMessage($missing_properties): string{
+        return sprintf(self::$ERROR_MSG_MISSING_PROPERTY, json_encode($missing_properties));
+    }
+
+}
