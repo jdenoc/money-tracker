c43caf163746aff53c4c9b15cd99e516c0fbeab6
jdenoc <jdenoc@gmail.com>
2021-11-10 11:37:44 -0600

    - Replaced the spinner component with another that is vue 3 compatible.

--- resources/js/components/loading-modal.vue
+++ resources/js/components/loading-modal.vue
@@ -3,58 +3,106 @@
         <div class="modal-background"></div>
         <div class="modal-content">
             <div class="container">
-                <spinner
-                    v-bind:depth="spinner.depth"
-                    v-bind:size="spinner.size"
-                    v-bind:speed="spinner.speed"
-                    v-bind:color="spinner.color"
-                ></spinner>
+              <vue-element-loading v-bind="loaderProperties" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    // utilities
     import randomColor from 'randomcolor/randomColor';
-    import Spinner from 'vue-spinner-component/src/Spinner.vue';
+    import _ from 'lodash';
+    // components
+    import VueElementLoading from "vue-element-loading";
 
     export default {
-        name: "loading-modal",
-        components: {
-            Spinner
+      name: "loading-modal",
+      components: {
+        VueElementLoading
+      },
+      data: function(){
+        return {
+          isLoading: false,
+          loadingColor: '',
+          loadingSpinnerProperties: {},
+        };
+      },
+      computed: {
+        loaderProperties: function(){
+          let defaultProperties = {
+            active: this.isLoading,
+            color: this.loadingColor,
+            isFullScreen: true,
+            backgroundColor: "transparent",
+          };
+          return _.merge(defaultProperties, this.loadingSpinnerProperties);
         },
-        data: function(){
-            return {
-                isLoading: false,
-                spinner: {
-                    depth: 5,
-                    size: 175,
-                    speed: 0.5,
-                    color: ""
-                },
-            };
+
+        // spinners
+        availableSpinners: function(){
+          return {
+            'spinner': this.spinnerSpinner,
+            'ring': this.spinnerRing,
+            'line-scale': this.spinnerLineScale,
+            'bar-fade-scale': this.spinnerBarFadeScale,
+          }
         },
-        methods: {
-            showLoading: function(){
-                this.generateRandomColor();
-                this.isLoading = true;
-            },
-            stopLoading: function(){
-                this.isLoading = false;
-            },
-            generateRandomColor: function(){
-                this.spinner.color = randomColor();
-            }
+        spinnerSpinner: function(){
+          return {
+            spinner: 'spinner',
+            duration: "0.5",  // seconds is takes to complete one animation
+            size: '196',      // px
+          }
         },
-        created: function(){
-            this.$eventBus.listen(this.$eventBus.EVENT_LOADING_SHOW(), this.showLoading);
-            this.$eventBus.listen(this.$eventBus.EVENT_LOADING_HIDE(), this.stopLoading);
+        spinnerRing: function(){
+          return {
+            spinner: 'ring',
+            duration: '0.6',  // seconds is takes to complete one animation
+            size: '196',      // px
+          }
         },
+        spinnerLineScale: function(){
+          return {
+            spinner: 'line-scale',
+            duration: '0.8',  // seconds is takes to complete one animation
+            size: '144',      // px
+          }
+        },
+        spinnerBarFadeScale: function(){
+          return {
+            spinner: 'bar-fade-scale',
+            duration: '1.0',  // seconds is takes to complete one animation
+            size: '176',      // px
+          }
+        },
+      },
+      methods: {
+        showLoading: function(){
+          this.randomiseLoader();
+          this.isLoading = true;
+        },
+        stopLoading: function(){
+          this.isLoading = false;
+        },
+        randomiseLoader: function(){
+          this.generateRandomColor();
+          this.setRandomSpinnerProperties();
+        },
+        setRandomSpinnerProperties: function(){
+          this.loadingSpinnerProperties = _.sample(this.availableSpinners);
+        },
+        generateRandomColor: function(){
+          this.loadingColor = randomColor();
+        }
+      },
+      created: function(){
+          this.$eventBus.listen(this.$eventBus.EVENT_LOADING_SHOW(), this.showLoading);
+          this.$eventBus.listen(this.$eventBus.EVENT_LOADING_HIDE(), this.stopLoading);
+      },
     }
 </script>
 
 <style scoped>
-    .container {
-        width: 100px;
-    }
+
 </style>
