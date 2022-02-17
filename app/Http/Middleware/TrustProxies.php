9854428a710d4b205165dece0ecc14155edb2e74
jdenoc <jdenoc@gmail.com>
2022-02-17 09:31:56 -0600

    Bumping to Laravel 7.x

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,7 +10,7 @@ class TrustProxies extends Middleware
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var array|string
+     * @var array|string|null
      */
     protected $proxies;
 
