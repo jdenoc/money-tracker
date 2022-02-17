c253a63fd995deb43e8a4e1f92c52783998d7580
jdenoc <jdenoc@gmail.com>
2022-02-17 09:31:56 -0600

    Bumping to Laravel 7.x

--- database/migrations/2020_08_15_145047_create_failed_jobs_table.php
+++ database/migrations/2020_08_15_145047_create_failed_jobs_table.php
@@ -15,7 +15,7 @@ class CreateFailedJobsTable extends Migration {
      */
     public function up(){
         Schema::create(self::$TABLE, function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
