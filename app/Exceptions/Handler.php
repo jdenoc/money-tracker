8833d7037c5ee8a54051f967503b858bd849a7fc
jdenoc <jdenoc@gmail.com>
2022-03-18 20:49:30 -0500

    Bumping to Laravel 8.x

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Request;
 use Symfony\Component\HttpFoundation\Response as HttpStatus;
 use Throwable;
 
@@ -13,7 +14,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that should not be reported.
      *
-     * @var array
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -22,53 +23,51 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $dontFlash = [
+        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Throwable  $exception
      * @return void
-     *
-      * @throws \Exception
      */
-    public function report(\Throwable $exception)
-    {
-        parent::report($exception);
-    }
+    public function register(){
+        $this->reportable(function (Throwable $e) {
+            //
+        });
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Exception
-     */
-    public function render($request, Throwable $exception){
-        if ($exception instanceof TokenMismatchException && $request->is('attachment/upload')){
+        $this->renderable(function(TokenMismatchException $e, Request $request){
             // handle TokenMismatchException's for attachment/upload
-            return response(['error' => "token mis-match"], HttpStatus::HTTP_UNAUTHORIZED);
+            if($request->is('attachment/upload')){
+                return response()->json(
+                    ['error' => "token mis-match"],
+                    HttpStatus::HTTP_UNAUTHORIZED
+                );
+            }
+        });
 
-        } elseif($exception instanceof PostTooLargeException && $request->is('attachment/upload')){
-            return response(
-                ['error'=>'The uploaded file exceeds your post_max_size ini directive.'],
-                HttpStatus::HTTP_REQUEST_ENTITY_TOO_LARGE
-            );
-        } elseif($request->is('attachment/upload')){
-            return response(
-                ['error'=>'Error occurred during upload. Contact admin.'],
-                HttpStatus::HTTP_INTERNAL_SERVER_ERROR
-            );
-        } else {
-            return parent::render($request, $exception);
-        }
+        $this->renderable(function(PostTooLargeException $e, Request $request){
+            if($request->is('attachment/upload')){
+                return response()->json(
+                    ['error'=>'The uploaded file exceeds your post_max_size ini directive.'],
+                    HttpStatus::HTTP_REQUEST_ENTITY_TOO_LARGE
+                );
+            }
+        });
+
+        $this->renderable(function(\Exception $e, Request $request){
+            if($request->is('attachment/upload')){
+                return response()->json(
+                    ['error'=>'Error occurred during upload. Contact admin.'],
+                    HttpStatus::HTTP_INTERNAL_SERVER_ERROR
+                );
+            }
+        });
     }
 
 }
