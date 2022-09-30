559895be14e61f3d68e92d3182cb59ffa4070430
jdenoc <jdenoc@gmail.com>
2022-09-29 21:00:15 -0500

    Utilising tailwind color names for generating tag names when testing.

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Jdenoc\Faker\TailwindColors\Provider as TailwindColorsProvider;
 
 class TagFactory extends Factory {
 
     public function definition():array {
+        $this->faker->addProvider(new TailwindColorsProvider($this->faker));
         return [
-            'name'=>$this->faker->unique()->colorName(),
+            'name'=>$this->faker->unique()->tailwindColorName(),
         ];
     }
 
