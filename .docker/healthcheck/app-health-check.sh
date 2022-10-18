8b4db0cccda742dcaca985e5c31b42b6d1e6f844
jdenoc <jdenoc@gmail.com>
2022-10-17 23:44:07 -0500

    corrected health-check script for the app.money-tracker container

--- .docker/healthcheck/app-health-check.sh
+++ .docker/healthcheck/app-health-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-http_code=$(curl --silent --write-out '%{http_code}' --output /var/log/php/healthcheck.log http://localhost/api/version)
+http_code=$(curl --silent --write-out '%{http_code}' --output /dev/null http://localhost/api/version)
 
 if [[ $http_code -eq 200 ]]; then
   exit 0  # success
