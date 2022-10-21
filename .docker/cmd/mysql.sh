940260cd99b4b7e1bfcf0d4ff6664eb25ce6906f
jdenoc <jdenoc@gmail.com>
2022-10-21 00:34:40 -0500

    Moved `docker-compose.yml` into the root directory.

--- .docker/cmd/mysql.sh
+++ .docker/cmd/mysql.sh
@@ -7,31 +7,7 @@ if [[ ! -f ${ENV_FILE} ]]; then
 	exit
 fi
 
-OPTIONS=$1
-FLAGS=-i
-
-case "$OPTIONS" in
-  -h|--help|?)
-    echo "Usage:"
-    echo "  $0 [options]"
-    echo "  echo \"show tables;\" | $0"
-    echo "  $0 | < /path/to/import/file.sql"
-    echo "Options:"
-    echo "  -h|--help|?         display this \"help\" message"
-    echo "  -i|--interactive    allows access to the mysql command in an interactive state"
-    echo ""
-    exit
-  ;;
-  -i|--interactive)
-    # TTY interface is required to directly interact with mysql command line
-    # BUT it can NOT be used if you are passing commands to or importing files to mysql.
-    FLAGS+=t
-    shift
-    shift
-  ;;
-esac
-
-docker container exec $FLAGS mysql.money-tracker mysql \
+docker-compose exec database mysql \
   -u"$(grep DB_USERNAME $ENV_FILE | sed 's/DB_USERNAME=//')" \
   -p"$(grep DB_PASSWORD $ENV_FILE | sed 's/DB_PASSWORD=//')" \
   "$(grep DB_DATABASE $ENV_FILE | sed 's/DB_DATABASE=//')"
