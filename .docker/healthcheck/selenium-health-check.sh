4c1b7a76238bc71febfa16c94b21056be316e06a
jdenoc <jdenoc@gmail.com>
2022-10-21 21:09:20 -0500

    Reduced complexity of the selenium health check script.

--- .docker/healthcheck/selenium-health-check.sh
+++ .docker/healthcheck/selenium-health-check.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-iteration=0
-max_iterations=10
-
-while [[ $iteration -lt $max_iterations ]]; do
-  if [[ "$( curl -s http://selenium:4444/wd/hub/status | jq -r .value.ready )" == "true" ]]; then
-    exit 0
-  else
-    iteration=$(( iteration+1 ))
-    sleep 1
-  fi;
-done
-
-exit 1
+if [[ "$( curl -s http://selenium:4444/wd/hub/status | jq -r .value.ready )" == "true" ]]; then
+  exit 0  # success
+else
+  exit 1  # failure
+fi
