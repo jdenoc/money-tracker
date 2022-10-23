ff3c1512a5f1da2bc20db39e37eb447e99db6246
jdenoc <jdenoc@gmail.com>
2022-10-22 21:43:44 -0500

    Fixed downloads path used by export tests.

--- app/Traits/ExportsHelper.php
+++ app/Traits/ExportsHelper.php
@@ -6,19 +6,10 @@ use Illuminate\Support\Facades\Storage;
 
 trait ExportsHelper {
 
-    private static $STORAGE_DOWNLOAD_DIR = 'downloads/';
+    private static string $STORAGE_DOWNLOAD_DIR = 'downloads/';
 
-    private $absolute_download_dir;
-
-    protected function setAbsoluteDownloadDir(){
-        $this->absolute_download_dir = Storage::disk(self::$TEST_STORAGE_DISK_NAME)->path(self::$STORAGE_DOWNLOAD_DIR);
-    }
-
-    protected function getAbsoluteDownloadDir(){
-        if(is_null($this->absolute_download_dir)){
-            throw new \InvalidArgumentException("setAbsoluteDownloadDir() was not called");
-        }
-        return $this->absolute_download_dir;
+    protected function getAbsoluteDownloadDir():string{
+        return Storage::disk(self::$TEST_STORAGE_DISK_NAME)->path(self::$STORAGE_DOWNLOAD_DIR);
     }
 
     protected function generateExportFilename():string{
@@ -29,7 +20,7 @@ trait ExportsHelper {
         return ['ID','Date','Memo','Income','Expense','AccountType','Attachment','Transfer','Tags'];
     }
 
-    protected function clearStorageDownloadDir(){
+    protected function clearStorageDownloadDir():void{
         foreach(Storage::disk(self::$TEST_STORAGE_DISK_NAME)->files(self::$STORAGE_DOWNLOAD_DIR) as $f){
             if(!str_contains($f, '.gitignore')){
                 Storage::disk(self::$TEST_STORAGE_DISK_NAME)->delete($f);
