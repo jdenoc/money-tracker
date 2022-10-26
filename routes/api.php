742db0167f633961fd1d6e43dc377cffbe7248c1
jdenoc <jdenoc@gmail.com>
2022-10-26 10:19:51 -0500

    Changed how routing is handled.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AccountController;
+use App\Http\Controllers\Api\AccountTypeController;
+use App\Http\Controllers\Api\AttachmentController;
+use App\Http\Controllers\Api\EntryController;
+use App\Http\Controllers\Api\InstitutionController;
+use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\VersionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,101 +25,101 @@ use Illuminate\Support\Facades\Route;
 //});
 
 // GET /api/version
-Route::get('version', 'Api\VersionController@get')
+Route::get('version', [VersionController::class, 'get'])
     ->name('version');
 
 // GET /api/institutions
-Route::get('institutions', 'Api\InstitutionController@get_institutions')
+Route::get('institutions', [InstitutionController::class,'get_institutions'])
     ->name('institutions');
 // GET /api/institutes
-Route::get('institutes', 'Api\InstitutionController@get_institutions')
+Route::get('institutes', [InstitutionController::class,'get_institutions'])
     ->name('institutes');
 // GET /api/institution/{institution_id}
-Route::get('institution/{institution_id}', 'Api\InstitutionController@get_institution')
+Route::get('institution/{institution_id}', [InstitutionController::class,'get_institution'])
     ->name('institution');
 // GET /api/institute/{institution_id}
-Route::get('institute/{institution_id}', 'Api\InstitutionController@get_institution')
+Route::get('institute/{institution_id}', [InstitutionController::class,'get_institution'])
     ->name('institute');
-Route::put('institution/{institution_id}', 'Api\InstitutionController@update_institution')
+Route::put('institution/{institution_id}', [InstitutionController::class,'update_institution'])
     ->name('institution.put');
-Route::put('institute/{institution_id}', 'Api\InstitutionController@update_institution')
+Route::put('institute/{institution_id}', [InstitutionController::class,'update_institution'])
     ->name('institute.put');
-Route::post('institution', 'Api\InstitutionController@create_institution')
+Route::post('institution', [InstitutionController::class,'create_institution'])
     ->name('institution.post');
-Route::post('institute', 'Api\InstitutionController@create_institution')
+Route::post('institute', [InstitutionController::class,'create_institution'])
     ->name('institute.post');
 
 // GET /api/tags
-Route::get('tags', 'Api\TagController@get_tags')
+Route::get('tags', [TagController::class, 'get_tags'])
     ->name('tags');
 // POST /api/tag
-Route::post('tag', 'Api\TagController@createTag')
+Route::post('tag', [TagController::class, 'createTag'])
     ->name('tag.post');
 // PUT /api/tag/{tag_id}
-Route::put('tag/{tag_id}', 'Api\TagController@updateTag')
+Route::put('tag/{tag_id}', [TagController::class, 'updateTag'])
     ->name('tag.put');
 
 // GET /api/accounts
-Route::get('accounts', 'Api\AccountController@get_accounts')
+Route::get('accounts', [AccountController::class, 'get_accounts'])
     ->name('accounts');
 // GET /api/account/{account_id}
-Route::get('account/{account_id}', 'Api\AccountController@get_account')
+Route::get('account/{account_id}', [AccountController::class, 'get_account'])
     ->name('account.get');
 // POST /api/account
-Route::post('account', 'Api\AccountController@create_account')
+Route::post('account', [AccountController::class, 'create_account'])
     ->name('account.post');
 // PUT /api/account/{account_id}
-Route::put('account/{account_id}', 'Api\AccountController@update_account')
+Route::put('account/{account_id}', [AccountController::class, 'update_account'])
     ->name('account.put');
 
 // GET /api/account-types
-Route::get('account-types', 'Api\AccountTypeController@list_account_types')
+Route::get('account-types', [AccountTypeController::class, 'list_account_types'])
     ->name('account_types.get');
 // GET /api/account-types/types
-Route::get('account-types/types', 'Api\AccountTypeController@list_account_type_types')
+Route::get('account-types/types', [AccountTypeController::class, 'list_account_type_types'])
     ->name('account-type.types.get');
 // GET /api/account-type/{account_type_id}
-Route::get('account-type/{account_type_id}', 'Api\AccountTypeController@get_account_type')
+Route::get('account-type/{account_type_id}', [AccountTypeController::class, 'get_account_type'])
     ->name('account_type.get');
 // DELETE /api/account-type/{account_type_id}
-Route::delete('account-type/{account_type_id}', 'Api\AccountTypeController@disable_account_type')
+Route::delete('account-type/{account_type_id}', [AccountTypeController::class, 'disable_account_type'])
     ->name('account_type.delete');
 // POST /api/account-type
-Route::post('account-type', 'Api\AccountTypeController@create_account_type')
+Route::post('account-type', [AccountTypeController::class, 'create_account_type'])
     ->name('account_type.post');
 // PUT /api/account-type/{account_type_id}
-Route::put('account-type/{account_type_id}', 'Api\AccountTypeController@update_account_type')
+Route::put('account-type/{account_type_id}', [AccountTypeController::class, 'update_account_type'])
     ->name('account_type.put');
 
 // GET /api/entries
-Route::get('entries', 'Api\EntryController@get_paged_entries')
+Route::get('entries', [EntryController::class, 'get_paged_entries'])
     ->name('entries.get');
 // GET /api/entries/{page}
-Route::get('entries/{page}', 'Api\EntryController@get_paged_entries')
+Route::get('entries/{page}', [EntryController::class, 'get_paged_entries'])
     ->name('entries.get.paged');
 // POST /api/entries
-Route::post('entries', 'Api\EntryController@filter_paged_entries')
+Route::post('entries', [EntryController::class, 'filter_paged_entries'])
     ->name('entries.post');
 // POST /api/entries/{page}
-Route::post('entries/{page}', 'Api\EntryController@filter_paged_entries')
+Route::post('entries/{page}', [EntryController::class, 'filter_paged_entries'])
     ->name('entries.post.paged');
 
 // POST /api/entry
-Route::post('entry', 'Api\EntryController@create_entry')
+Route::post('entry', [EntryController::class, 'create_entry'])
     ->name('entry.post');
 // GET /api/entry/{entry_id}
-Route::get('entry/{entry_id}', 'Api\EntryController@get_entry')
+Route::get('entry/{entry_id}', [EntryController::class, 'get_entry'])
     ->name('entry.get');
 // PUT /api/entry/{entry_id}
-Route::put('entry/{entry_id}', 'Api\EntryController@update_entry')
+Route::put('entry/{entry_id}', [EntryController::class, 'update_entry'])
     ->name('entry.put');
 // DELETE /api/entry/{entry_id}
-Route::delete('entry/{entry_id}', 'Api\EntryController@delete_entry')
+Route::delete('entry/{entry_id}', [EntryController::class, 'delete_entry'])
     ->name('entry.delete');
 // POST /api/entry/transfer
-Route::post('entry/transfer', 'Api\EntryController@create_transfer_entries')
+Route::post('entry/transfer', [EntryController::class, 'create_transfer_entries'])
     ->name('entry.transfer');
 
 // DELETE /api/attachment/{uuid}
-Route::delete('attachment/{uuid}', 'Api\AttachmentController@delete_attachment')
+Route::delete('attachment/{uuid}', [AttachmentController::class, 'delete_attachment'])
     ->name('attachment.delete');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Web\AttachmentController;
+use App\Http\Controllers\Web\ExportsController;
+use App\Http\Controllers\Web\HomeController;
+use App\Http\Controllers\Web\SettingsController;
+use App\Http\Controllers\Web\StatsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/laravel', 'Web\HomeController@laravelWelcome')->name('laravel-welcome');
-Route::get('/', 'Web\HomeController@display');
-Route::get('/home', 'Web\HomeController@display');
-Route::get('/stats', 'Web\StatsController@display');
-Route::get('/settings', 'Web\SettingsController@display');
-Route::get('/attachment/{uuid}', 'Web\AttachmentController@display');
-Route::post('/attachment/upload', 'Web\AttachmentController@upload');
-Route::delete('/attachment/upload', 'Web\AttachmentController@deleteUpload');
-Route::post('/export', 'Web\ExportsController@export');
+Route::get('/laravel', [HomeController::class, 'laravelWelcome'])->name('laravel-welcome');
+Route::get('/', [HomeController::class, 'display']);
+Route::get('/home', [HomeController::class, 'display']);
+Route::get('/stats', StatsController::class);
+Route::get('/settings', SettingsController::class);
+Route::get('/attachment/{uuid}', [AttachmentController::class, 'display']);
+Route::post('/attachment/upload', [AttachmentController::class, 'upload']);
+Route::delete('/attachment/upload', [AttachmentController::class, 'deleteUpload']);
+Route::post('/export', ExportsController::class);
