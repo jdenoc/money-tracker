eb30635a4620a44b5b21ac81197d28f68ff19d81
jdenoc <jdenoc@gmail.com>
2022-10-25 20:37:11 -0500

    Updated to mysql8.

--- database/migrations/2015_09_23_014041_create_stamp_trigger.php
+++ database/migrations/2015_09_23_014041_create_stamp_trigger.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateStampTrigger extends Migration {
 
-    private static $TRIGGER = 'trigger_entry_creation_timestamp';
+    private static string $TRIGGER = 'trigger_entry_creation_timestamp';
 
     /**
      * Create a MySQL trigger to create a trigger on the entries table to add the current time to the entries.create_stamp when inserting a new record
-     *
-     * @return void
      */
     public function up(){
         DB::unprepared(
@@ -22,8 +21,6 @@ class CreateStampTrigger extends Migration {
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down(){
         DB::unprepared("DROP TRIGGER IF EXISTS ".self::$TRIGGER);
