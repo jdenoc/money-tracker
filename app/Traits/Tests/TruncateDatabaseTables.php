e70d6b8c7a15fbe102aa4ffb747704bec88b0a99
jdenoc <jdenoc@gmail.com>
2022-10-27 21:36:19 -0500

    Adjusted the TruncateDatabaseTables trait to account for tables with foreign key constraints.

--- app/Traits/Tests/TruncateDatabaseTables.php
+++ app/Traits/Tests/TruncateDatabaseTables.php
@@ -9,8 +9,12 @@ trait TruncateDatabaseTables {
     /**
      * Truncates all database tables related to this connection, except for the "migrations" table
      * @link http://stackoverflow.com/a/18910102/4152012
+     * Allow truncating tables with forenign keys
+     * @link https://stackoverflow.com/a/5452798/4152012
      */
     private function truncateDatabaseTables(array $ignore_tables=[]){
+        $query_set_foreign_key_checks = "SET FOREIGN_KEY_CHECKS = %d;";
+        DB::statement(DB::raw(sprintf($query_set_foreign_key_checks, 0)));
         $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
         foreach($tables as $table){
             if(in_array($table, $ignore_tables)){
@@ -19,6 +23,7 @@ trait TruncateDatabaseTables {
             }
             DB::table($table)->truncate();
         }
+        DB::statement(DB::raw(sprintf($query_set_foreign_key_checks, 1)));
     }
 
 }
