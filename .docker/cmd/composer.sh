0105a7c4eb6259c0201f4a5e1f00d78159b14bdb
jdenoc <jdenoc@gmail.com>
2022-11-16 22:14:27 -0600

    Forcing Github Actions to cache the composer docker image in the build step.

--- .docker/cmd/composer.sh
+++ .docker/cmd/composer.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+IMAGE_TAG=composer-local
 if [[ "$(docker images -q composer-local 2> /dev/null)" == "" ]]; then
   DOCKERFILE_PATH=$(dirname $(realpath $0))/../composer.dockerfile
   CONTEXT_PATH=$(dirname $(realpath $0))/../../
-  docker build --rm --file $DOCKERFILE_PATH --tag composer-local $CONTEXT_PATH
+  docker build --rm --file "$DOCKERFILE_PATH" --tag $IMAGE_TAG "$CONTEXT_PATH"
 fi
 
-docker container run --rm -t --volume "$PWD":/app composer-local "$@"
+docker container run --rm -t --volume "$PWD":/app $IMAGE_TAG "$@"
