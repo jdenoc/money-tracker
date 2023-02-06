b8abdc5588412edf1a5ac08e02c1b2b6c9963f91
jdenoc <jdenoc@gmail.com>
2023-02-06 10:43:40 -0600

    Corrected AccountType cache

--- app/Models/AccountType.php
+++ app/Models/AccountType.php
@@ -66,12 +66,12 @@ class AccountType extends BaseModel {
             CacheEntity::make(self::CACHE_KEY_ALL)
                 ->forever()
                 ->cache(function() {
-                    return AccountType::all();
+                    return AccountType::withTrashed()->get();
                 }),
             CacheEntity::make(self::CACHE_KEY_COUNT)
                 ->forever()
                 ->cache(function() {
-                    return AccountType::count();
+                    return AccountType::withTrashed()->count();
                 }),
             CacheEntity::make(self::CACHE_KEY_TYPES)
                 ->forever()
