c945ca1212bf09c87fa11ee20043ad6e22b87dc7
jdenoc <jdenoc@gmail.com>
2023-05-04 14:18:18 -0500

    If the version is NOT set, then a 204 HTTP Code response is returned.

--- app/Http/Controllers/Api/VersionController.php
+++ app/Http/Controllers/Api/VersionController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response as HttpStatus;
 
 class VersionController extends Controller {
 
     const CONFIG_VERSION = "app.version";
 
     public function get() {
-        return response(config(self::CONFIG_VERSION));
+        $version = config(self::CONFIG_VERSION);
+        $http_code = empty($version) ? HttpStatus::HTTP_NO_CONTENT : HttpStatus::HTTP_OK;
+        return response($version, $http_code);
     }
 
 }
