349782c38899a0c527efe878536fcd19af04eaac
jdenoc <jdenoc@gmail.com>
2023-05-22 21:31:03 -0500

    Instead of checking to see if the application service is running for the composer process, just run a new container for the composer process.

--- .docker/scripts/composer.sh
+++ .docker/scripts/composer.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ $(docker-compose ps application --status=running | tail -n+2 | wc -l) -eq 1 ]]; then
-  # container is running
-  docker-compose exec -T application composer "$@"
-else
-  # container NOT running
-  docker-compose run --rm --no-deps -T application composer "$@"
-fi
+docker-compose run --rm --no-deps -T application composer "$@"
