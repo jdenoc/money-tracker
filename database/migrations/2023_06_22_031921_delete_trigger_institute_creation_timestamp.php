d4a7054e684d39d07324590399b75c1934029bd9
jdenoc <jdenoc@gmail.com>
2023-06-21 22:43:15 -0500

    Modified institutions table timestamps.

---
+++ database/migrations/2023_06_22_031921_delete_trigger_institute_creation_timestamp.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Reverse of 2017_06_26_174109_create_trigger_institute_creation_timestamp.php
+ */
+return new class extends Migration {
+
+    private const TRIGGER = 'trigger_institute_creation_timestamp';
+
+    /**
+     * Run the migrations.
+     */
+    public function up() {
+        DB::unprepared("DROP TRIGGER IF EXISTS ".self::TRIGGER);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down() {
+        DB::unprepared(
+            "CREATE TRIGGER ".self::TRIGGER."
+            BEFORE INSERT ON institutions
+            FOR EACH ROW
+            SET NEW.create_stamp = NOW()"
+        );
+    }
+
+};
