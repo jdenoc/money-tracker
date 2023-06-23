57803e16d6a851b6d321630ea8bc2e15dbeae462
jdenoc <jdenoc@gmail.com>
2023-06-23 15:03:38 -0500

    Allowed deleted entries to be accessibly directly through the api.

--- tests/Feature/Api/Delete/DeleteEntryTest.php
+++ tests/Feature/Api/Delete/DeleteEntryTest.php
@@ -21,26 +21,56 @@ class DeleteEntryTest extends TestCase {
 
         // WHEN
         $get_response1 = $this->get($this->_base_uri.$entry->id);
+
+        // THEN
+        $this->assertResponseStatus($get_response1, HttpStatus::HTTP_OK);
+
+        // WHEN
         $delete_response = $this->delete($this->_base_uri.$entry->id);
         $get_response2 = $this->get($this->_base_uri.$entry->id);
 
         // THEN
-        $this->assertResponseStatus($get_response1, HttpStatus::HTTP_OK);
         $this->assertResponseStatus($delete_response, HttpStatus::HTTP_NO_CONTENT);
-        $this->assertResponseStatus($get_response2, HttpStatus::HTTP_NOT_FOUND);
+        $this->assertResponseStatus($get_response2, HttpStatus::HTTP_OK);
 
         $this->assertEmpty($delete_response->getContent());
         $this->assertIsArray($get_response2->json());
-        $this->assertEmpty($get_response2->json());
+        $this->assertNotEmpty($get_response2->json());
 
         // confirm disabled_stamp and modified_stamp have been updated
         $newly_disabled_entry = Entry::withTrashed()->find($entry->id);
+        $this->assertEquals($newly_disabled_entry->{Entry::DELETED_AT}, $get_response2[Entry::DELETED_AT]);
         $this->assertNotNull($newly_disabled_entry->{Entry::DELETED_AT}, Entry::DELETED_AT.' value is unexpected ['.$newly_disabled_entry->{Entry::DELETED_AT}.']');
         $this->assertDateFormat($newly_disabled_entry->{Entry::DELETED_AT}, Carbon::ATOM, $newly_disabled_entry->{Entry::DELETED_AT}. "does not match format ".Carbon::ATOM);
-        $this->assertNotEmpty($entry->{Entry::UPDATED_AT}, Entry::UPDATED_AT.' value is empty ['.$newly_disabled_entry->{Entry::UPDATED_AT}.']');
+        $this->assertEquals($newly_disabled_entry->{Entry::UPDATED_AT}, $get_response2[Entry::UPDATED_AT]);
         $this->assertNotEmpty($newly_disabled_entry->{Entry::UPDATED_AT}, Entry::UPDATED_AT.' value is empty ['.$newly_disabled_entry->{Entry::UPDATED_AT}.']');
         $this->assertDateFormat($newly_disabled_entry->{Entry::UPDATED_AT}, Carbon::ATOM, $newly_disabled_entry->{Entry::UPDATED_AT}. "does not match format ".Carbon::ATOM);
+        $this->assertNotEmpty($entry->{Entry::UPDATED_AT}, Entry::UPDATED_AT.' value is empty ['.$newly_disabled_entry->{Entry::UPDATED_AT}.']');
         $this->assertNotEquals($entry->{Entry::UPDATED_AT}, $newly_disabled_entry->{Entry::UPDATED_AT});
+    }
+
+    public function testMarkingPreviouslyDeletedEntryToBeDeleted() {
+        // GIVEN
+        $entry = Entry::factory()
+            ->for(AccountType::factory()->for(Account::factory()))
+            ->disabled()
+            ->create();
+
+        // WHEN
+        $get_response = $this->get($this->_base_uri.$entry->id);
+
+        // THEN
+        $this->assertResponseStatus($get_response, HttpStatus::HTTP_OK);
+        $this->assertIsArray($get_response->json());
+        $this->assertNotEmpty($get_response->json());
+        $this->assertNotNull($get_response[Entry::DELETED_AT]);
+
+        // WHEN
+        $delete_response = $this->delete($this->_base_uri.$entry->id);
+
+        // THEN
+        $this->assertResponseStatus($delete_response, HttpStatus::HTTP_NOT_FOUND);
+        $this->assertEmpty($delete_response->getContent());
     }
 
     public function testMarkingEntryDeletedWhenEntryDoesNotExist() {
@@ -49,13 +79,17 @@ class DeleteEntryTest extends TestCase {
 
         // WHEN
         $get_response = $this->get($this->_base_uri.$entry_id);
-        $delete_response = $this->delete($this->_base_uri.$entry_id);
 
         // THEN
         $this->assertResponseStatus($get_response, HttpStatus::HTTP_NOT_FOUND);
-        $this->assertResponseStatus($delete_response, HttpStatus::HTTP_NOT_FOUND);
         $this->assertIsArray($get_response->json());
         $this->assertEmpty($get_response->json());
+
+        // WHEN
+        $delete_response = $this->delete($this->_base_uri.$entry_id);
+
+        // THEN
+        $this->assertResponseStatus($delete_response, HttpStatus::HTTP_NOT_FOUND);
         $this->assertEmpty($delete_response->getContent());
     }
 
