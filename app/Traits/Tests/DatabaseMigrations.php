d27930e446936011cbf1758e6bb2b0e62516e85c
jdenoc <jdenoc@gmail.com>
2023-06-22 20:00:43 -0500

    Added functionality to allow the re-use of the latest database snapshot in tests

--- app/Traits/Tests/DatabaseMigrations.php
+++ app/Traits/Tests/DatabaseMigrations.php
@@ -11,37 +11,53 @@ trait DatabaseMigrations {
     protected static bool $FRESH_RUN = true;
     protected static string $SNAPSHOT_NAME = '';
 
-    public function migrate(bool $with_seeder=true) {
-        if (self::$FRESH_RUN) {
-            // init a listener to clean up file
-            Event::listen(CreatedSnapshot::class, function(CreatedSnapshot $event) {
-                $snapshot_file_path = $event->snapshot->disk->path($event->snapshot->fileName);
-                $file_contents = file_get_contents($snapshot_file_path);
-                $file_contents = str_replace(';;', ';', $file_contents);
-                $file_contents = str_replace('DELIMITER ;', "\n", $file_contents);
-                $db_username = config('database.connections.'.config('database.default').'.username');
-                $file_contents = str_replace("/*!50017 DEFINER=`$db_username`@`%`*/", "", $file_contents);
-                file_put_contents($snapshot_file_path, $file_contents);
-                self::$DUMP_READY = true;
-            });
-
-            // load database migrations and seed with data
-            $options = ($with_seeder) ? ['--seeder'=>'UiSampleDatabaseSeeder'] : [];
-            $this->artisan('migrate:fresh', $options);
-
-            // dump database contents to file
-            self::$SNAPSHOT_NAME = class_basename(get_called_class());
-            $this->artisan('snapshot:create', ['name'=>self::$SNAPSHOT_NAME]);
-
-            self::$FRESH_RUN = false;
+    public function migrate(bool $withSeeder=true) {
+        if (config('testing.snapshot.use_latest')) {
+            $this->migrateWithLatestSnapshot();
         } else {
-            while (!self::$DUMP_READY) {
-                // check every second until snapshot is ready
-                sleep(1);
+            if (self::$FRESH_RUN) {
+                $this->migrateFresh($withSeeder);
+            } else {
+                $this->migrateWithSpecificSnapshot();
             }
-            // init database from file
-            $this->artisan('snapshot:load', ['name'=>self::$SNAPSHOT_NAME]);
         }
+    }
+
+    private function migrateWithLatestSnapshot() {
+        $this->artisan('snapshot:load', ['--latest'=>true]);
+    }
+
+    private function migrateFresh(bool $withSeeder) {
+        // init a listener to clean up file
+        Event::listen(CreatedSnapshot::class, function(CreatedSnapshot $event) {
+            $snapshot_file_path = $event->snapshot->disk->path($event->snapshot->fileName);
+            $file_contents = file_get_contents($snapshot_file_path);
+            $file_contents = str_replace(';;', ';', $file_contents);
+            $file_contents = str_replace('DELIMITER ;', "\n", $file_contents);
+            $db_username = config('database.connections.'.config('database.default').'.username');
+            $file_contents = str_replace("/*!50017 DEFINER=`$db_username`@`%`*/", "", $file_contents);
+            file_put_contents($snapshot_file_path, $file_contents);
+            self::$DUMP_READY = true;
+        });
+
+        // load database migrations and seed with data
+        $options = ($withSeeder) ? ['--seeder'=>'UiSampleDatabaseSeeder'] : [];
+        $this->artisan('migrate:fresh', $options);
+
+        // dump database contents to file
+        self::$SNAPSHOT_NAME = class_basename(get_called_class());
+        $this->artisan('snapshot:create', ['name'=>self::$SNAPSHOT_NAME]);
+
+        self::$FRESH_RUN = false;
+    }
+
+    private function migrateWithSpecificSnapshot() {
+        while (!self::$DUMP_READY) {
+            // check every second until snapshot is ready
+            sleep(1);
+        }
+        // init database from file
+        $this->artisan('snapshot:load', ['name'=>self::$SNAPSHOT_NAME]);
     }
 
     public static function cleanup() {
