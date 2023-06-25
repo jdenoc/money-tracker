cd9bd568a6816a5b348e39645d199fcb5fabb8c9
jdenoc <jdenoc@gmail.com>
2023-06-25 01:11:18 -0500

    Moved one test from the `api-post` testsuite to the `api-delete` testsuite.

--- tests/Feature/Api/Delete/DeleteEntryTest.php
+++ tests/Feature/Api/Delete/DeleteEntryTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature\Api\Delete;
 use App\Models\Account;
 use App\Models\AccountType;
 use App\Models\Entry;
+use App\Models\Institution;
+use Brick\Money\Money;
 use Carbon\Carbon;
 use Symfony\Component\HttpFoundation\Response as HttpStatus;
 use Tests\TestCase;
@@ -90,6 +92,46 @@ class DeleteEntryTest extends TestCase {
         // THEN
         $this->assertResponseStatus($delete_response, HttpStatus::HTTP_NOT_FOUND);
         $this->assertEmpty($delete_response->getContent());
+    }
+
+    public function testDeletingEntryUpdatesAccountTotal() {
+        // GIVEN
+        $generated_account = Account::factory()->for(Institution::factory())
+            ->create();
+        $generated_account_type = AccountType::factory()->for($generated_account)->create();
+        $generated_entry = Entry::factory()->for($generated_account_type)
+            ->create();
+
+        // WHEN
+        $get_account_response1 = $this->get('/api/account/'.$generated_account->id);
+        // THEN
+        $get_account_response1->assertStatus(HttpStatus::HTTP_OK);
+        $get_account_response1_as_array = $get_account_response1->json();
+        $this->assertArrayHasKey('total', $get_account_response1_as_array);
+        $this->assertArrayHasKey('currency', $get_account_response1_as_array);
+        $original_total = Money::of($get_account_response1_as_array['total'], $get_account_response1_as_array['currency']);
+
+        // WHEN
+        $delete_response = $this->delete(sprintf(self::BASE_URI, $generated_entry->id));
+        // THEN
+        $delete_response->assertStatus(HttpStatus::HTTP_NO_CONTENT);
+        $this->assertEmpty($delete_response->getContent());
+
+        // WHEN
+        $get_account_response2 = $this->get('/api/account/'.$generated_account->id);
+        // THEN
+        $get_account_response2->assertStatus(HttpStatus::HTTP_OK);
+        $get_account_response2_as_array = $get_account_response2->json();
+        $this->assertArrayHasKey('total', $get_account_response2_as_array);
+        $this->assertArrayHasKey('currency', $get_account_response2_as_array);
+        $updated_total = Money::of($get_account_response2_as_array['total'], $get_account_response2_as_array['currency']);
+
+        $entry_value = Money::of($generated_entry->entry_value, $generated_account->currency)
+            ->multipliedBy(($generated_entry->expense ? -1 : 1));
+        $this->assertTrue(
+            $original_total->minus($entry_value)->isEqualTo($updated_total),
+            "Entry value:".$entry_value."; Original total:".$original_total."; Updated total:".$updated_total
+        );
     }
 
 }
