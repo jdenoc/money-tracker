effa39dc6a8f4df47afc0f0d5a01fec99e2e1181
jdenoc <jdenoc@gmail.com>
2024-08-05 22:57:28 -0500

    feat: update mysql docker container

--- .docker/healthcheck/mysql-health-check.sh
+++ .docker/healthcheck/mysql-health-check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mysqladmin ping --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" \
-  && mysql --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --database="$MYSQL_DATABASE" --skip-column-names  -e"SELECT NOW(); SHOW TABLES;"
+mysqladmin ping --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" 2> /dev/null \
+  && mysql --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --database="$MYSQL_DATABASE" --skip-column-names  -e"SELECT NOW(); SHOW TABLES;" 2> /dev/null
