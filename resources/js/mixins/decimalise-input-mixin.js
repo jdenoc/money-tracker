3380d95d3f0687803c96338ee16f0bfc454ed978
jdenoc <jdenoc@gmail.com>
2024-08-13 22:43:17 -0500

    fix: fixed entry value decimalisation when displayed in the entry-modal.

--- resources/js/mixins/decimalise-input-mixin.js
+++ resources/js/mixins/decimalise-input-mixin.js
@@ -4,12 +4,7 @@ export const decimaliseInputMixin = {
 
   methods: {
     decimaliseValue: function(inputValue){
-      if(!_.isEmpty(inputValue)){
-        let sanitisedValue = inputValue.replace(/[^\d.-]/g, '');
-        return parseFloat(sanitisedValue).toFixed(2);
-      } else {
-        return '';
-      }
+      return _.toNumber(inputValue).toFixed(2);
     }
   }
 };
