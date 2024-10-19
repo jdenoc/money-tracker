ac21b7c4a6ce3125a4a0fbf05934f38d2453cdcc
jdenoc <jdenoc@gmail.com>
2024-10-18 20:37:44 -0500

    perf: Bumped rate limit back up to 500 requests/minute to `api` routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider {
      */
     public function boot(): void {
         RateLimiter::for('api', function(Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(500)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function() {
