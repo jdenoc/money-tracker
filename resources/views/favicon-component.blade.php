9e2343635d0d28d019fb8cad78f502047275aaed
jdenoc <jdenoc@gmail.com>
2024-11-25 23:38:11 -0600

    fix(favicon): moved favicon files

--- resources/views/favicon-component.blade.php
+++ resources/views/favicon-component.blade.php
@@ -1,5 +1,6 @@
 <!-- favicon -->
-<link rel="apple-touch-icon" sizes="180x180" href="{{ favicon('imgs/favicon/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ favicon('imgs/favicon/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ favicon('imgs/favicon/favicon-16x16.png') }}">
-<link rel="manifest" href="{{ 'imgs/favicon/site.webmanifest' }}">
+<link rel="shortcut icon" type="image/x-icon"  href="'favicon/favicon.ico'">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ favicon('favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ favicon('favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ favicon('favicon/favicon-16x16.png') }}">
+<link rel="manifest" href="favicon/site.webmanifest">
